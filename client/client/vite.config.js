812000c29a03f2d6555be897b94a44449ec89c24
luckygbear-coder <luckygbear@gmail.com>
2025-12-01 22:28:28 +0800

    Add Vite configuration for GitHub Pages deployment

---
+++ client/client/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // 這裡要放「GitHub 專案名稱」
+  // 你的網址是 https://luckygbear-coder.github.io/Hero.little/
+  // 所以 base 就是 "/Hero.little/"
+  base: '/Hero.little/'
+})
